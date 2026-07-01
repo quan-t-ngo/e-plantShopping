1988ed4ed8e6ed6cd8d34aba1000101feef25b90
Quan T. Ngo <90666138+quan-t-ngo@users.noreply.github.com>
2026-07-01 20:53:54 +0700

    Create initial App component with landing page

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="landing-page">
+      <h1 className="landing-title">Paradise Nursery</h1>
+      <button className="start-button">Get Started</button>
+    </div>
+  );
+};
+
+export default App;
